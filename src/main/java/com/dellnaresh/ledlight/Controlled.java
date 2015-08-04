46aaaa07548fabec8cd3b100077973931a455a29
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2015-08-05 00:02:53 +1000

    working code

--- src/main/java/com/dellnaresh/ledlight/Controlled.java
+++ src/main/java/com/dellnaresh/ledlight/Controlled.java
@@ -1,59 +1,141 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+package com.dellnaresh.ledlight; /**
+ * Class to monitor distance measured by an HC-SR04 distance sensor on a 
+ * Raspberry Pi.
+ *
+ * The main method assumes the trig pin is connected to the pin # 7 and the echo
+ * pin is connected to pin # 11.  Output of the program are comma separated lines
+ * where the first value is the number of milliseconds since unix epoch, and the
+ * second value is the measured distance in centimeters.
  */
-package com.dellnaresh.ledlight;
 
-import com.pi4j.io.gpio.*;
-import com.pi4j.io.gpio.trigger.GpioCallbackTrigger;
-
-import java.util.concurrent.Callable;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.RaspiPin;
 
 /**
+ * DistanceMonitor class to monitor distance measured by sensor
  *
- * @author nareshm
+ * @author Rutger Claes <rutger.claes@cs.kuleuven.be>
  */
-public class Controlled{
+public class Controlled {
 
-    public static void main(String[] args) throws InterruptedException {
+    private final static float SOUND_SPEED = 340.29f;  // speed of sound in m/s
 
-        System.out.printf("PIR Module Test (CTRL+C to exit)\n");
+    private final static int TRIG_DURATION_IN_MICROS = 10; // trigger duration of 10 micro s
+    private final static int WAIT_DURATION_IN_MILLIS = 60; // wait 60 milli s
 
-        // create gpio controller
-        final GpioController gpio = GpioFactory.getInstance();
-        // provision gpio pin #29, (header pin 40) as an input pin with its internal pull down resistor enabled
-        final GpioPinDigitalInput pir = gpio.provisionDigitalInputPin(RaspiPin.GPIO_29);
-        // provision gpio pin #01 as an output pin and turn on
-        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "MyLED", PinState.LOW);
-        System.out.printf("Ready\n");
+    private final static int TIMEOUT = 2100;
 
-        // create a gpio callback trigger on the gpio pin
-        Callable<Void> callback = new Callable<Void>() {
-            @Override
-            public Void call() throws Exception {
-                System.out.println(" --> GPIO TRIGGER CALLBACK RECEIVED ");
-                pin.toggle();
-                return null;
-            }
-        };
-        // create a gpio callback trigger on the PIR device pin for when it's state goes high
-        pir.addTrigger(new GpioCallbackTrigger(PinState.HIGH, callback));
+    private final static GpioController gpio = GpioFactory.getInstance();
 
-        // stop all GPIO activity/threads by shutting down the GPIO controller
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                System.out.println("Interrupted, stopping...\n");
-                gpio.shutdown();
-            }
-        });
+    private final GpioPinDigitalInput echoPin;
+    private final GpioPinDigitalOutput trigPin;
 
-        // keep program running until user aborts (CTRL-C)
-        for (;;) {
-            Thread.sleep(100);
+    private Controlled( Pin echoPin, Pin trigPin ) {
+        this.echoPin = gpio.provisionDigitalInputPin( echoPin );
+        this.trigPin = gpio.provisionDigitalOutputPin( trigPin );
+        this.trigPin.low();
+    }
+
+    /*
+     * This method returns the distance measured by the sensor in cm
+     * 
+     * @throws TimeoutException if a timeout occurs
+     */
+    public float measureDistance() throws TimeoutException {
+        this.triggerSensor();
+        this.waitForSignal();
+        long duration = this.measureSignal();
+
+        return duration * SOUND_SPEED / ( 2 * 10000 );
+    }
+
+    /**
+     * Put a high on the trig pin for TRIG_DURATION_IN_MICROS
+     */
+    private void triggerSensor() {
+        try {
+            this.trigPin.high();
+            Thread.sleep( 0, TRIG_DURATION_IN_MICROS * 1000 );
+            this.trigPin.low();
+        } catch (InterruptedException ex) {
+            System.err.println( "Interrupt during trigger" );
+        }
+    }
+
+    /**
+     * Wait for a high on the echo pin
+     *
+     */
+    private void waitForSignal() throws TimeoutException {
+        int countdown = TIMEOUT;
+
+        while( this.echoPin.isLow() && countdown > 0 ) {
+            countdown--;
         }
 
+        if( countdown <= 0 ) {
+            throw new TimeoutException( "Timeout waiting for signal start" );
+        }
+    }
+
+    /**
+     * @return the duration of the signal in micro seconds
+     */
+    private long measureSignal() throws TimeoutException {
+        int countdown = TIMEOUT;
+        long start = System.nanoTime();
+        while( this.echoPin.isHigh() && countdown > 0 ) {
+            countdown--;
+        }
+        long end = System.nanoTime();
+
+        if( countdown <= 0 ) {
+            throw new TimeoutException( "Timeout waiting for signal end" );
+        }
+
+        return (long)Math.ceil( ( end - start ) / 1000.0 );  // Return micro seconds
+    }
+
+    public static void main( String[] args ) {
+        Pin echoPin = RaspiPin.GPIO_28;
+        Pin trigPin = RaspiPin.GPIO_29;
+        Controlled monitor = new Controlled( echoPin, trigPin );
+
+        while( true ) {
+            try {
+                System.out.printf( "%1$d,%2$.3f%n", System.currentTimeMillis(), monitor.measureDistance() );
+            }
+            catch( TimeoutException e ) {
+                System.err.println( e );
+            }
+
+            try {
+                Thread.sleep( WAIT_DURATION_IN_MILLIS );
+            } catch (InterruptedException ex) {
+                System.err.println( "Interrupt during trigger" );
+            }
+        }
+    }
+
+    /**
+     * Exception thrown when timeout occurs
+     */
+    private static class TimeoutException extends Exception {
+
+        private final String reason;
+
+        public TimeoutException( String reason ) {
+            this.reason = reason;
+        }
+
+        @Override
+        public String toString() {
+            return this.reason;
+        }
     }
 
 }
--- src/main/java/com/dellnaresh/ledlight/ControlledSensor.java
+++ src/main/java/com/dellnaresh/ledlight/ControlledSensor.java
@@ -11,50 +11,50 @@ import com.pi4j.io.gpio.trigger.GpioCallbackTrigger;
 import java.util.concurrent.Callable;
 
 /**
- *
+ * Sensor used HCSR-501 (Motion Sensor)
  * @author nareshm
+ * @see <a http://www.oracle.com/technetwork/articles/java/cruz-gpio-2295970.html></a>
  */
-public class ControlledSensor {
-    
-        public static void main(String[] args) throws InterruptedException {
+public class ControlledSensor{
 
-            System.out.printf("PIR Module Test (CTRL+C to exit)\n");
+    public static void main(String[] args) throws InterruptedException {
 
-            // create gpio controller
-            final GpioController gpio = GpioFactory.getInstance();
-            // provision gpio pin #29, (header pin 40) as an input pin with its internal pull down resistor enabled
-            final GpioPinDigitalInput pir = gpio.provisionDigitalInputPin(RaspiPin.GPIO_07);
-            // provision gpio pin #01 as an output pin and turn on
-            final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "MyLED", PinState.LOW);
-            System.out.printf("Ready\n");
+        System.out.printf("PIR Module Test (CTRL+C to exit)\n");
 
-            // create a gpio callback trigger on the gpio pin
-            Callable<Void> callback = new Callable<Void>() {
-                @Override
-                public Void call() throws Exception {
-                    System.out.println(" --> GPIO TRIGGER CALLBACK RECEIVED ");
-                    pin.high();
-                    Thread.sleep(5000);
-                    return null;
-                }
-            };
-            // create a gpio callback trigger on the PIR device pin for when it's state goes high
-            pir.addTrigger(new GpioCallbackTrigger(PinState.HIGH, callback));
+        // create gpio controller
+        final GpioController gpio = GpioFactory.getInstance();
+        // provision gpio pin #29, (header pin 40) as an input pin with its internal pull down resistor enabled
+        final GpioPinDigitalInput pir = gpio.provisionDigitalInputPin(RaspiPin.GPIO_29);
+        // provision gpio pin #01 as an output pin and turn on
+        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "MyLED", PinState.LOW);
+        System.out.printf("Ready\n");
 
-            // stop all GPIO activity/threads by shutting down the GPIO controller
-            Runtime.getRuntime().addShutdownHook(new Thread() {
-                @Override
-                public void run() {
-                    System.out.println("Interrupted, stopping...\n");
-                    gpio.shutdown();
-                }
-            });
-
-            // keep program running until user aborts (CTRL-C)
-            for (;;) {
-                Thread.sleep(100);
+        // create a gpio callback trigger on the gpio pin
+        Callable<Void> callback = new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+                System.out.println(" --> GPIO TRIGGER CALLBACK RECEIVED ");
+                pin.toggle();
+                return null;
             }
+        };
+        // create a gpio callback trigger on the PIR device pin for when it's state goes high
+        pir.addTrigger(new GpioCallbackTrigger(PinState.HIGH, callback));
 
+        // stop all GPIO activity/threads by shutting down the GPIO controller
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Interrupted, stopping...\n");
+                gpio.shutdown();
+            }
+        });
+
+        // keep program running until user aborts (CTRL-C)
+        for (;;) {
+            Thread.sleep(100);
         }
+
+    }
 
 }
